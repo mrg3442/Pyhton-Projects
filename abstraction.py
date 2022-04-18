f58f8a0326e3592950011b4ef12294101fc94aeb
mrg3442 <101129274+mrg3442@users.noreply.github.com>
2022-04-18 04:09:21 -0400

    Create abstraction.py

---
+++ abstraction.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self): pass
+
+    @abstractmethod
+    def perimeter(self): pass
+
+class Sqaure(Shape):
+    def __init__(self, side):
+        self.__side = side
+
+
+    def area(self):
+        return self.__side * self.__side
+    def perimeter(self):
+        return 4 * self.__side
+
+square = Sqaure(5)
+print(square.area())
+print(square.perimeter())
